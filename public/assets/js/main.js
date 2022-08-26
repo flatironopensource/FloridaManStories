ffa8e378543869a5742f52f24699882788d45611
Umut Hope YILDIRIM <umut475@gmail.com>
2022-08-26 00:37:19 -0400

    duddddddsdd

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -235,24 +235,28 @@ document.addEventListener("DOMContentLoaded", () => {
 
 });
 
-let allNews = document.querySelectorAll('allNews')
-allNews.onscroll = (event) => {
-  console.log('Scrolling...');  
-  fetch('https://floridamanstories.ml/api/getnewslromlastsentnews', {
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-      },
-      method: "POST",
-      body: JSON.stringify({
-        lastNews : lastNewsId
-      })
-  })
-  .then(response => response.json())
-  .then(data => {
-    lastNews+=data.length
-    data.forEach(item => {
-      addAllNews(item._fieldsProto.urlToImage.stringValue,item._fieldsProto.title.stringValue,item._fieldsProto.description.stringValue,item._fieldsProto.shortUrl.stringValue,item._ref._path.segments[1],item._fieldsProto.likes.integerValue);
+$(window).bind('mousewheel', function(event) {
+  if (event.originalEvent.wheelDelta >= 0) {
+      console.log('Scroll up');
+  }
+  else {
+    console.log('Scroll down');
+     fetch('https://floridamanstories.ml/api/getnewslromlastsentnews', {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+        },
+        method: "POST",
+        body: JSON.stringify({
+          lastNews : lastNewsId
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+      lastNews+=data.length
+      data.forEach(item => {
+        addAllNews(item._fieldsProto.urlToImage.stringValue,item._fieldsProto.title.stringValue,item._fieldsProto.description.stringValue,item._fieldsProto.shortUrl.stringValue,item._ref._path.segments[1],item._fieldsProto.likes.integerValue);
+      });
     });
-  });
-};
+  }
+});
