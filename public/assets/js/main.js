16501964abc0363e4b5c1b4bcfdd1b490d3b34aa
Umut Hope YILDIRIM <umut475@gmail.com>
2022-08-26 02:32:29 -0400

    its window not windows

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -264,7 +264,7 @@ document.addEventListener("DOMContentLoaded", () => {
 let lastScrollTop = 0;
 
 // element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
-windows.addEventListener("scroll", function(){ // or window.addEventListener("scroll"....
+window.addEventListener("scroll", function(){ // or window.addEventListener("scroll"....
    let st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
    if (st > lastScrollTop){
     console.log('Scroll down');
